70ac588677d88b7fdee696334e2210d04c1a1b16
Talal Khan <talal.khan@jintech.com>
2023-08-21 00:14:28 +0500

    TK - Update the UI design of Hero and Stats

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,13 +6,21 @@ const Hero = () => {
 
     return(
         <div className="hero-image">
-            <img 
+            {/* <img 
                 src={heroImage}
                 alt="the-insane-studio hero-image"
             />
             <div className="overlay">
                 <h1 className="different-heading">Let us shape your<br/> digital success story!</h1>
                 <button className="button">Explore Works</button>
+            </div> */}
+            <div className="hero-heading">
+                <h1 className="different-heading">Let us shape your<br/> digital success story!</h1>
+                <button className="button">Explore Works</button>
+            </div>
+            <div className="hero-video">
+                <iframe src="https://www.youtube.com/embed?v=Wmd6ix9xcLE" width={'100%'} height={450} frameborder={"no"} style={{borderRadius:'20px'}}></iframe>              
+                <div className="play-pause-button"></div>
             </div>
         </div>
     )
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,12 +3,17 @@
 
 const Stats = () => {
     return (
-        <div class="container">
-            <div class="box"><span className="c-purple">Clients</span>100+ Satisfied</div>
-            <div class="line"></div>
-            <div class="box"><span className="c-purple">Projects</span>400+ Delivered</div>
-            <div class="line"></div>
-            <div class="box"><span className="c-purple">Projects</span>100+</div>
+        // <div class="container">
+        //     <div class="box"><span className="c-purple">Clients</span>100+ Satisfied</div>
+        //     <div class="line"></div>
+        //     <div class="box"><span className="c-purple">Projects</span>400+ Delivered</div>
+        //     <div class="line"></div>
+        //     <div class="box"><span className="c-purple">Projects</span>100+</div>
+        // </div>
+        <div className="stats">
+            <div><span className="c-purple">Clients</span> 100+ Satisfied</div>
+            <div><span className="c-purple">Projects</span> 400+ Delivered</div> 
+            <div><span className="c-purple">Reviews</span> 100+</div>
         </div>
     )
 }
