1c939d914bde1bf73d07ce3ff225223259205c52
Mubashir Ahmed Malik <mubashir.ahmed@jintech.com>
2023-08-31 15:58:25 +0500

    updated testimonials styles

--- src/components/TestimonialsCarousel/Testimonial.js
+++ src/components/TestimonialsCarousel/Testimonial.js
@@ -1,16 +1,19 @@
 import React from "react";
 const Testimonial = ({ item }) => {
   return (
-    <div style={{ borderRadius: 50, backgroundColor: "black", padding: 50 }}>
+    <div style={{ borderRadius: 50, backgroundColor: "rgba(0, 0, 0, 0.5)", padding: 50 }}>
       <div style={{ padding: 10, display: "flex" }}>
         <img
-          style={{ borderRadius: 50, border: "4px solid #8ffd2a",  objectFit: "cover" }}
+          style={{ borderRadius: 50, border: "3px solid #8ffd2a",  objectFit: "cover" }}
           src={item.imageSrc}
           alt="the-insane-studio testimonials"
           width="60px"
           height="60px"
         />
-        <h4 style={{ marginLeft: 10, color: "white" }}>{item.by}</h4>
+        <div className="reviewer-about">
+            <div style={{ fontSize: '22px', fontWeight: 'bold' }}>{item.by}</div>
+            <div style={{ fontSize: '14px' }}>Founder/CEO</div>
+        </div>
       </div>
       <div>
         <p style={{ color: "white" }}>{item.message}</p>
