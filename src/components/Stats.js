d06c802f0a4bbc05a41d68e95746f1501d9061f6
Mubashir Ahmed Malik <mubashir.ahmed@jintech.com>
2023-09-13 12:13:02 +0500

    add counter animation in stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,23 +1,13 @@
-
-
+import CountUp from "react-countup"
 
 const Stats = () => {
     return (
-        // <div class="container">
-        //     <div class="box"><span className="c-purple">Clients</span>100+ Satisfied</div>
-        //     <div class="line"></div>
-        //     <div class="box"><span className="c-purple">Projects</span>400+ Delivered</div>
-        //     <div class="line"></div>
-        //     <div class="box"><span className="c-purple">Projects</span>100+</div>
-        // </div>
         <div className="stats">
-            <div><span className="c-purple">Clients</span> 100+ Satisfied</div>
-            <div><span className="c-purple">Projects</span> 400+ Delivered</div> 
-            <div><span className="c-purple">Reviews</span> 100+</div>
+            <div><span className="c-purple">Clients</span> <CountUp end={100} duration={5} />+ Satisfied</div>
+            <div><span className="c-purple">Projects</span> <CountUp end={400} duration={5} />+ Delivered</div> 
+            <div><span className="c-purple">Reviews</span> <CountUp end={100} duration={5} />+</div>
         </div>
     )
 }
-
-
 
 export default Stats
