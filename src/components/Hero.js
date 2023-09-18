d025493e843ad649ae1dc18020717f2403c0f4bf
Mubashir Ahmed Malik <mubashir.ahmed@jintech.com>
2023-09-19 00:40:24 +0500

    add hero video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,13 @@
-const Hero = () => {
+import heroVideo from '../assets/Hero Video.mp4'
 
+const Hero = () => {
 
     return(
         <div className="hero">
+             <video autoPlay muted loop id="video-background">
+                <source src={heroVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <div className="hero-header">
                 <h1>Let us<br/>shape your<br/> digital success<br/> story!</h1>
                 <button>Explore Work<i class="fa-solid fa-arrow-right"></i></button>
