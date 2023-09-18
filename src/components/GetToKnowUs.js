0b4354d5fcd1616d54e786b96e0b38fcc607910f
AMMAR HAIDER <101063742+Ammar-18-MCT-06@users.noreply.github.com>
2023-09-18 14:55:08 +0500

    Update GetToKnowUs.js

--- src/components/GetToKnowUs.js
+++ src/components/GetToKnowUs.js
@@ -8,8 +8,7 @@ const GetToKnowUs = () => {
             <div className="first-div">
                 <h1 className="heading">Get To Know About Us Closer</h1>
                 <div className="get-to-know-us-text">
-                    
-Founded in 2023, Insane Studios is a visionary leader in the realm of digital services. Our mission is to deliver nothing short of excellence in the digital landscape. Comprising a team of dedicated and industrious professionals, we have become synonymous with top-tier workmanship in Pakistan.
+                    Founded in 2023, Insane Studios is a visionary leader in the realm of digital services. Our mission is to deliver nothing short of excellence in the digital landscape. Comprising a team of dedicated and industrious professionals, we have become synonymous with top-tier workmanship in Pakistan.
                 </div>
                 <button style={{ width: "270px" }}>Explore Our Portfolio</button>
             </div>
