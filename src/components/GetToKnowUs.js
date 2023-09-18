e68159c613205c3b340afff2268731c1bf21a12c
AMMAR HAIDER <101063742+Ammar-18-MCT-06@users.noreply.github.com>
2023-09-18 14:54:34 +0500

    Update GetToKnowUs.js

--- src/components/GetToKnowUs.js
+++ src/components/GetToKnowUs.js
@@ -8,7 +8,8 @@ const GetToKnowUs = () => {
             <div className="first-div">
                 <h1 className="heading">Get To Know About Us Closer</h1>
                 <div className="get-to-know-us-text">
-                    is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    
+Founded in 2023, Insane Studios is a visionary leader in the realm of digital services. Our mission is to deliver nothing short of excellence in the digital landscape. Comprising a team of dedicated and industrious professionals, we have become synonymous with top-tier workmanship in Pakistan.
                 </div>
                 <button style={{ width: "270px" }}>Explore Our Portfolio</button>
             </div>
